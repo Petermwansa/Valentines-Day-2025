5ea552535408b3e18ce32a8170d1c9a132cd596e
Petermwansa <mwansapeter800@gmail.com>
2025-02-10 22:41:19 +0300

    Add all the code after finishing and polishing up all the features and animations

---
+++ script.js
@@ -0,0 +1,39 @@
+const increaseYesButton = () => {
+    let yesButton = document.querySelector('.yes');
+    let currentSize = window.getComputedStyle(yesButton).getPropertyValue('transform');
+    let scaleMatch = currentSize.match(/matrix\(([^,]+),/);
+    let scale = scaleMatch ? parseFloat(scaleMatch[1]) : 1;
+    yesButton.style.transform = `scale(${scale + 0.2})`;
+}
+
+
+
+const createHeart = () => {
+    let heart = document.createElement("div");
+    heart.innerHTML = "❤️";
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    document.body.appendChild(heart);
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+setInterval(createHeart, 300);
+
+
+const showLove = () => {
+    let bigHeart = document.createElement("div");
+    bigHeart.innerHTML = "❤️";
+    bigHeart.classList.add("big-heart");
+    document.body.appendChild(bigHeart);
+
+    
+    // we create a dive to display the heart 
+    let loveText = document.createElement("div");
+    loveText.innerHTML = "Thank You! <br> I Love You! <br> Baby Girl <br> Melochka";
+    loveText.classList.add("love-text");
+    document.body.appendChild(loveText);
+
+
+}
